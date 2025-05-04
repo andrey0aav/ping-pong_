27a3823056231f792a026ff5b2b7be309989e00b
andrey0aav <avaleksandriy@gmail.com>
2025-05-04 17:23:07 +0700

    Create msin.py

---
+++ msin.py
@@ -0,0 +1,77 @@
+import pygame
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+WIDTH = 600
+HEIGHT = 600
+pygame.init()
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+    # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        # если мяч достигает границ экрана, меняем направление его движения
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        # если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        # если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        racket1.reset()
