1e805b53542a3b82538a0da605fceadcb8fc8f2d
andrey0aav <avaleksandriy@gmail.com>
2025-05-18 17:14:44 +0700

    Update msin.py

--- msin.py
+++ msin.py
@@ -1,54 +1,66 @@
-import pygame
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+from pygame import *
 
-WIDTH = 600
-HEIGHT = 600
-pygame.init()
-# класс-родитель для других спрайтов
+back = (150, 159, 0)
+win_width = 600
+win_height = 600
+
+
+window = display.set_mode((win_height, win_width))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+speed_x = 3
+speed_y = 3
+font.init()
+
+
 class GameSprite(sprite.Sprite):
-    # конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # Вызываем конструктор класса (Sprite):
-        sprite.Sprite.__init__(self)
 
-        # каждый спрайт должен хранить свойство image - изображение
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
-
-        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
-    # метод, отрисовывающий героя на окне
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-# класс главного игрока
 class Player(GameSprite):
-    # метод для управления спрайтом стрелками клавиатуры
-    def update_l(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 
+
+racket1 = Player('img.png', 30, 200, 50, 100, 15)
+racket2 = Player('img.png', 520, 200, 50, 100, 15)
+ball = GameSprite('img.png', 200, 200, 30, 30, 50)
+
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-
+    if finish != True:
+        window.fill(back)
         racket1.update_l()
         racket2.update_r()
         ball.rect.x += speed_x
@@ -58,20 +70,25 @@ while game:
             speed_x *= -1
             speed_y *= 1
 
-        # если мяч достигает границ экрана, меняем направление его движения
+
         if ball.rect.y > win_height - 50 or ball.rect.y < 0:
             speed_y *= -1
 
-        # если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+
         if ball.rect.x < 0:
             finish = True
             window.blit(lose1, (200, 200))
             game_over = True
 
-        # если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+
         if ball.rect.x > win_width:
             finish = True
             window.blit(lose2, (200, 200))
             game_over = True
 
         racket1.reset()
+        racket2.reset()
+        ball.reset()
+
+    display.update()
+    clock.tick(FPS)
